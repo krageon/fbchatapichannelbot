5b29d5144853f51c8c5cfb875205888dc6ac6edf
Tim Bazuin <krageon@gmail.com>
2013-02-20 20:09:10 +0100

    made this a VS project

--- botmain.py
+++ botmain.py
@@ -1,1 +1,12 @@
 import pyxmpp2
+
+# TODO: Create IO system similar to IRSSI
+
+# TODO: Make interface to Facebook (handle protocol crap)
+##  Create hooks for event-based handling
+
+# TODO: Make chat interface to Facebook
+
+# TODO: Make bot interface to Facebook
+##  Use events to generate pattern-based responses (ie resolving songs to youtube)
+##  Operate inside chat interface
